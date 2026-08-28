e219ced6fe1a8ad158bfdfaeb6d270be74a4aab9
phegondev <phegondev@gmail.com>
2026-08-28 20:10:08 +0100

    added prof frontend url

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ app = FastAPI(title="Shopease", version="0.1.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:4200"],
+    allow_origins=["http://localhost:3000", "*", "http://deploy-shopease923.s3-website.eu-north-1.amazonaws.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
